fcd9c9a5f98ab06b8d60cb6192176127f7c58df4
damienien <d.choquet@hotmail.com>
2023-10-20 11:23:42 +0200

    refacto of the plusminus file

--- course/plusminus.py
+++ course/plusminus.py
@@ -8,23 +8,23 @@ def menu():
     print('Facile - entre 0 et 10 (1)')
     print('Moyen - entre 0 et 100 (2)')
     print('Difficile - entre 0 et 1000 (3)')
-    choose = input('Choisissez un niveau de diffculté: ')
+    choice = input('Choisissez un niveau de diffculté: ')
     global result
     
     try:
-        int(choose)
+        int(choice)
     except ValueError:
         print('Merci de choisir un nombre valide !')
         print('')
         return menu()
     else:
-        new_choose = int(choose)
-        if(new_choose != 1 and new_choose !=2 and new_choose !=3 ):
+        new_choice = int(choice)
+        if(new_choice != 1 and new_choice !=2 and new_choice !=3 ):
             print('Merci de choisir un niveau de difficulté valide : 1, 2 ou 3.')
             print('')
             return menu()
         else:
-            match(new_choose):
+            match(new_choice):
                 case 1:
                     result = random.randint(1, 10)
                     plusminus(1, 10, result)
@@ -38,22 +38,22 @@ def menu():
     
 
 def plusminus(val1, val2, result):
-    t = input(f"Entrer un nombre entre {val1} et {val2} : ")
+    choice = input(f"Entrer un nombre entre {val1} et {val2} : ")
     global essai
     
     try:
-        int(t)
+        int(choice)
     except ValueError:
         print('Ce n\'est pas un nombre')
         return plusminus(val1, val2, result)
     else:
-        new_t = int(t)
-        if(new_t > result):
-            print(f'Le nombre est plus petit que {new_t}')
+        new_choice = int(choice)
+        if(new_choice > result):
+            print(f'Le nombre est plus petit que {new_choice}')
             essai += 1
             return plusminus(val1, val2, result)
-        elif(new_t < result):
-            print(f'Le nombre est plus grand que {new_t}')
+        elif(new_choice < result):
+            print(f'Le nombre est plus grand que {new_choice}')
             essai += 1
             return plusminus(val1, val2, result)
         else:
